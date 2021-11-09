26f8399f96d9dd732850a4e7fbc6d5e0ae19303e
dblock <dblock@amazon.com>
2021-11-09 15:33:20 -0500

    Extract into a library instead of a vars.
    Signed-off-by: dblock <dblock@amazon.com>

--- vars/addJenkinsMessage.groovy
+++
@@ -1,5 +0,0 @@
-/** Add a message to the jenkins queue */
-def call(String stage, String message) {
-    writeFile(file: "notifications/${stage}.msg", text: message)
-    stash(includes: "notifications/*" , name: "notifications-${stage}")
-}
